87a62d28d06eb0f283472d801fdbc5eeaf7dafe0
tianye <tianye@davidTian.(none)>
2012-09-24 02:57:49 -0700

    modify height

--- leadsec_new/view/test.php
+++ leadsec_new/view/test.php
@@ -1,9 +1,33 @@
-<?php
-    echo '<hr/>';
-    echo '<hr/>';
-    echo '<hr/>';
-    echo '<hr/>';
-    echo '<hr/>';
-    echo '<hr/>';
-    echo '<hr/>';
-?>
+<!-- tabs -->
+<div class="tabs">
+    <ul>
+        <li>网络配置</li>
+        <li>Second</li>
+        <li>First</li>
+    </ul>
+</div>
+<div class="tabinfo">
+    &nbsp;&nbsp;<img src="../images/hi.gif" />&nbsp;
+    <span id="show_text">headline</span>
+</div>
+<br class="clearFloat"/>
+<!-- mainContent -->
+<div class="mainContent">
+    <div class="table">
+        <div class="column column_20 colTitle_1">
+            名称&nbsp;
+        </div>
+        <div class="column column_80 colTitle_2">
+            内容
+        </div>
+    
+        <br class="clearFloat"/>
+        <div class="column column_20 leftTitle_1">
+            安全隔离网闸序列号&nbsp;
+        </div>
+        <div class="column column_75 secCol">
+            123
+        </div>
+        <br class="clearFloat"/>
+    </div>
+</div>
