2fe5e00e7822bac2ea06ef768e57d1ea96320177
davidTian <123@123.com>
2012-10-29 13:52:17 +0800

    review commit ae043f2171ec1f8822cfcf05474e27b169ea3d75

--- leadsec/webUI/webserver/nginx/html/Lib/driver/userlog.php
+++ leadsec/webUI/webserver/nginx/html/Lib/driver/userlog.php
@@ -7,7 +7,7 @@ class Userlog {
 	 * @param unknown_type $module 操作模块
 	 * @param unknown_type $msg 信息描述
 	 */
-    public function __construct($time,$user,$module,$msg) {
+    public function __construct($time, $user, $module, $msg) {
     	//把数据存放在auth数据库中，新建一个userlog表中---有待进一步确定。
     }
 }
