363ef02f6b6611a722fa54e67bab564f0a1e12b5
tianye <Tianye5@leadsec.com.cn>
2012-10-31 03:26:13 -0700

    [function] set expired time

--- leadsec/webUI/webserver/nginx/html/Function/systemManagement/admin/account.php
+++ leadsec/webUI/webserver/nginx/html/Function/systemManagement/admin/account.php
@@ -1,5 +1,9 @@
 <?php
     @$expTime = $_POST['expTime'];
-    define('EXPIRED_TIME', $expTime);
-    echo EXPIRED_TIME;
+    $file = '/usr/local/webserver/nginx/html/Fragment/leadsec/webUI/webserver/nginx/html/Conf/global.php';
+    $contents = file_get_contents($file);
+    $contents = preg_replace("@\('EXPIRED_TIME',\s?(.*)\)@",
+        "('EXPIRED_TIME',$expTime)", $contents);
+    file_put_contents($file, $contents);
+    echo "<p>修改成功</p>";
 ?>
