00c4f9f566a589355b0cf164aa2cd8570e572274
tianye <Tianye5@leadsec.com.cn>
2012-11-19 04:47:44 -0800

    [pagination] resort data list when clicking button of reset page count

--- leadsec/webUI/webserver/nginx/html/Public/js/sort.js
+++ leadsec/webUI/webserver/nginx/html/Public/js/sort.js
@@ -1,25 +1,66 @@
-function sortTable(tableDom, headers, sortList, forceList) {
+function sortTableInit(tableDom, headers, sortList, forceList) {
     tableDom.tablesorter({
         headers: headers,
         sortList: sortList,
         sortMultiSortKey: 'ctrlKey',
         widgets: ['zebra'],
         sortInitialOrder : "asc",
-        sortForce: forceList
+        sortForce: forceList,
+        showProcessing: true
     });
 }
 
-function freshTable() {
-    var rowsCount = $('#rowsCount').val();
-    var pageNum   = $('.pagination>li.selected').text();
+function getOrderRules() {
+    // get rows count, val: 10, 20, 50, 100, all
+    var rowsCount = $('#rowsCount option:selected').val();
+    // get page num
+    var pageNum   = $('ol.pagination>li.selected').text();
+    // get sort key and order
+    var dataCount = $('#dataCount').val();
+    var sortData  = {};
     $('.tablesorter-headerRow>th').each(function() {
+        var key       = $(this).attr('name');
         var className = $(this).attr('class');
         if (className.indexOf('SortUp') !== -1) {
-            alert('DESC')
+            sortData[key] = 'DESC';
         } else if (className.indexOf('SortDown') !== -1) {
-            alert('ASC');
+            sortData[key] = 'ASC';
         } else {
             //no need to sort
         }
     });
+    return {
+        pageNum:   pageNum,
+        sortData:  sortData,
+        rowsCount: rowsCount
+    }
+}
+
+function resortTable(url, tableDom) {
+    var orderRules = getOrderRules();
+    var data = {
+        pageNum:   orderRules.pageNum,
+        sortData:  orderRules.sortData,
+        rowsCount: orderRules.rowsCount,
+    };
+    var params = {
+        success : function(result, textStatus) {
+            tableDom.children('tbody').html(result);
+            tableDom.trigger("update", [true]);
+        }
+    };
+    loadEmbedPage(url, data, tableDom.children('tbody'), params);
+}
+
+
+function freshPagination(displayDom, pageCount, clickedPageNo) {
+    if (!clickedPageNo) {
+        clickedPageNo = 1;
+    }
+    var url  = 'Function/layout/showPagination.php';
+    var data = {
+        pageCount: pageCount,
+        clickedPageNo: clickedPageNo
+    };
+    loadEmbedPage(url, data, displayDom);
 }
