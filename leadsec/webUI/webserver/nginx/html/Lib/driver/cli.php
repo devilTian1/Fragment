aba292f314ae852399e331cec0b2ac1af4cfb7c1
dt@home <spidertianye@gmail.com>
2012-11-25 15:10:19 +0800

    code format

--- leadsec/webUI/webserver/nginx/html/Lib/driver/cli.php
+++ leadsec/webUI/webserver/nginx/html/Lib/driver/cli.php
@@ -2,8 +2,7 @@
     require_once(WEB_PATH . '/Lib/core/cmd.php');
 
     class cli extends CMD {
-        public function __construct() {
-        }
+        public function __construct() {}
 
         public function run($cmd) {
             $result =  $this->exec($cmd);
