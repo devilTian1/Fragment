1c85fd6422e1665fd806a09a7ab88a12f38deee8
tianye <Tianye5@leadsec.com.cn>
2012-12-06 02:25:39 -0800

    [manufacture] small mistake

--- leadsec/webUI/webserver/nginx/html/manufacture.php
+++ leadsec/webUI/webserver/nginx/html/manufacture.php
@@ -4,14 +4,15 @@
     $msg = '等待生产文件上传';
     if (!empty($_FILES['manuFile'])) {
         // Manufacturea
+        // upload file
         $msg   = '生产失败.';
         $dPath = '/tmp';
         $fs    = new fileUpload($_FILES, $dPath);
         $fs->upload();
-
+        // run cli
         $cmd = 'manufact produce ' . $_FILES['manuFile']['name'];
         $cli = new cli();
-        //$cli->run($cmd);
+        $cli->run($cmd);
         $msg = 'success';
     } else if (!empty($_POST['download'])) {
         // Export manufact result
@@ -19,14 +20,14 @@
         header("Content-Disposition: attachment; filename=manufact_result.txt");
         readfile('/tmp/manufact_result');
     } else {
-        // init page data
-        $db = new dbsqlite(DB_PATH . '/private.db');
-        $result = $db->query("SELECT up_version FROM upgrade_history ORDER BY up_time DESC")
-                     ->getFirstData(PDO::FETCH_ASSOC);
-        $version = '没有任何升级记录';
-        if (false !== $result) {
-            $version = $result['up_version'];
-        }
+    }
+    // init page data
+    $db = new dbsqlite(DB_PATH . '/private.db');
+    $result = $db->query("SELECT up_version FROM upgrade_history ORDER BY up_time DESC")
+                 ->getFirstData(PDO::FETCH_ASSOC);
+    $version = '没有任何升级记录';
+    if (false !== $result) {
+        $version = $result['up_version'];
     }
 ?>
 <style>
