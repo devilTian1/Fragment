a54c3a2e62d09bd3eb959731c3f7a04505e1d903
tianye <Tianye5@leadsec.com.cn>
2012-12-06 23:45:26 -0800

    [manufacture]  delete useless dependent lib file

--- leadsec/webUI/webserver/nginx/html/manufacture.php
+++ leadsec/webUI/webserver/nginx/html/manufacture.php
@@ -1,5 +1,21 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/Function/common.php');
+    setlocale(LC_CTYPE, 'zh_CN.utf8');
+    // set exception handler
+    function exceptionHandler($e) {
+        echo json_encode(array('status' => -1, 'msg' => $e->getMessage()));
+    }
+    set_exception_handler('exceptionHandler');
+
+    // Load Config File
+    require_once('Conf/global.php');
+    // Exception driver
+    require_once(WEB_PATH . '/Lib/driver/exception.php');
+    // CLI driver
+    require_once(WEB_PATH . '/Lib/driver/cli.php');
+    // upload driver
+    require_once(WEB_PATH . '/Lib/driver/fileUpload.php');
+    // DB driver
+    require_once(WEB_PATH . '/Lib/driver/dbsqlite.php');
    
     $msg = '等待生产文件上传';
     if (!empty($_FILES['manuFile'])) {
@@ -12,13 +28,14 @@
         // run cli
         $cmd = 'manufact produce ' . $_FILES['manuFile']['name'];
         $cli = new cli();
-        $cli->run($cmd);
+        //$cli->run($cmd);
         $msg = 'success';
     } else if (!empty($_POST['download'])) {
         // Export manufact result
         header('Content-Type:download-force');
         header("Content-Disposition: attachment; filename=manufact_result.txt");
         readfile('/tmp/manufact_result');
+        return true;
     } else {
     }
     // init page data
@@ -30,45 +47,52 @@
         $version = $result['up_version'];
     }
 ?>
-<style>
-    #uploadManuFileForm {
-        display: inline
-    }
-</style>
-<table>
-    <tbody>
-        <tr>
-            <td>软件版本号 :</td>
-            <td><?php echo $version?></td>
-        </tr>
-        <tr>
-            <td>
-                配置文件:
-            </td>
-            <td>
-                <form method="POST" id="uploadManuFileForm" enctype="multipart/form-data">
-                    <input type="file" name="manuFile" id="manuFile"/>
-                    <button type="submit" onClick="openManuDialog()" id="manuBtn">
-                        开始生产</button>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <td>结果:</td>
-            <td>
-                <?php if('success' === $msg) {?>
-                    <form method="POST" id="uploadManuFileForm" enctype="multipart/form-data">
-                        <input type="hidden" name="download" value="1"/>
-                        <button type="submit">
-                            导出文件</button>
-                    </form>
-                <?php } else {
-                    echo $msg;
-                }?>
-            </td>
-        </tr>
-    </tbody>
-</table>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <style>
+            #uploadManuFileForm {
+                display: inline
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tbody>
+                <tr>
+                    <td>软件版本号 :</td>
+                    <td><?php echo $version?></td>
+                </tr>
+                <tr>
+                    <td>
+                        配置文件:
+                    </td>
+                    <td>
+                        <form method="POST" id="uploadManuFileForm" enctype="multipart/form-data">
+                            <input type="file" name="manuFile" id="manuFile"/>
+                            <button type="submit" onClick="openManuDialog()" id="manuBtn">
+                                开始生产</button>
+                        </form>
+                    </td>
+                </tr>
+                <tr>
+                    <td>结果:</td>
+                    <td>
+                        <?php if('success' === $msg) {?>
+                            <form method="POST" id="uploadManuFileForm" enctype="multipart/form-data">
+                                <input type="hidden" name="download" value="1"/>
+                                <button type="submit">
+                                    导出文件</button>
+                            </form>
+                        <?php } else {
+                            echo $msg;
+                        }?>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
 <script type="text/javascript">
     function openManuDialog() {
         if (document.getElementById('manuFile').value === '') {
