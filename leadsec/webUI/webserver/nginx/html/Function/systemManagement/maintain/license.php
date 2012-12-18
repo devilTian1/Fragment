6c93dd6ae7fe93066221dbd18e65d83c978dc24a
luobc <luobc@localhost.localdomain>
2012-12-18 01:28:00 -0800

    Product certificate upload module management

--- leadsec/webUI/webserver/nginx/html/Function/systemManagement/maintain/license.php
+++ leadsec/webUI/webserver/nginx/html/Function/systemManagement/maintain/license.php
@@ -1,17 +1,38 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'] . '/Function/common.php');
+    //get LicenseInformation  list
+    function GetLicenseInformation() {
+        $num = 1 ;
+        $db = new dbsqlite(DB_PATH . '/private.db');
+        $result = $db->query("SELECT * FROM license")->getAllData(PDO::FETCH_ASSOC);
+        //向数组里面追加键值对,同时判断value的字段值是否为1，
+        //若为1则表示该模块的状态为启用，为0则表示停止
+        //将截止时间戳转化为日期格式
+        foreach($result as $key => $val) {
+            if ($result[$key]['value']>0) {
+                $result[$key]['type']='启用';
+            } else {
+                $result[$key]['type']='停止';
+            }
+                $result[$key]['id'] = $num;
+                $result[$key]['stop_time']=date('Y-m-d',$result[$key]['stop_time']);
+                $num++ ;
+        }
+        V::getInstance()->assign('licenseInformation', $result) ;
+    }
 
     if (!empty($_FILES)) {
-        // Upgrade System
         $uploadfs = new fileUpload($_FILES);
         $uploadfs->upload();
-        $cmd = "upgrade package \"{$_FILES['licenseFile']['name']}\" " ;
+        //证书上传后要执行的命令
+        $path="/usr/local/license/bin/sls_upload";
+        $license="/tmp/".$_FILES['licenseFile']['name'] ;
+        $cmd=$path.' '.$license ;
         $cli = new cli();
         $cli->run($cmd);
         echo json_encode(array('msg' => '升级成功'));
     } else {
-
+        GetLicenseInformation() ;
     }
-
 ?>
 
--- leadsec/webUI/webserver/nginx/html/Tpls/templates/systemManagement/maintain/license.tpl
+++ leadsec/webUI/webserver/nginx/html/Tpls/templates/systemManagement/maintain/license.tpl
@@ -21,35 +21,28 @@
     </table>
 </form>
   
-   <table class="column_95 textMid tablesorter">
-   <caption>
-  License启用信息
-  </caption>
-    <tr>
-      <td class="column_10">序号</td>
-      <td class="column_30">模块名</td>
-      <td class="column_40">截止时间</td>
-      <td class="column_20">状态</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>定制访问</td>
-      <td>永不过期</td>
-      <td>启用</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>定制访问</td>
-      <td>永不过期</td>
-      <td>启用</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>定制访问</td>
-      <td>永不过期</td>
-      <td>启用</td>
-    </tr>
-  </table>
+<table class="column_95 textMid tablesorter">
+    <caption> License启用信息</caption>
+    <thead>
+        <tr>
+            <td class="column_10">序号</td>
+            <td class="column_30">模块名</td>
+            <td class="column_40">截止时间</td>
+            <td class="column_20">状态</td>
+        </tr>
+    </thead>
+    <{foreach $licenseInformation   as $k => $val }>
+        <tr>
+            <td><{$val.id}></td>
+            <td><{$val.application}></td>
+            <td><{$val.stop_time}></td>
+            <td><{$val.type}></td>
+        </tr>
+    <{foreachelse}>   
+        <tr><td colspan='4'>No Data</td></tr>
+    <{/foreach}>
+
+</table>
 <script type="text/javascript" src="Public/js/systemManagement/maintain/license.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
