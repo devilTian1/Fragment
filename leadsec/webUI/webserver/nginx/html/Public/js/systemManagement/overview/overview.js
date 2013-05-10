d970c28a490654dbad08cb9fb2eba21b24cbc051
xiaozl <xiaozelan@126.com>
2013-05-10 12:06:25 +0800

    cpu利用率的情况窗口根据不同数目变化

--- leadsec/webUI/webserver/nginx/html/Public/js/systemManagement/overview/overview.js
+++ leadsec/webUI/webserver/nginx/html/Public/js/systemManagement/overview/overview.js
@@ -1,3 +1,4 @@
+var opendialog;
 function openAllCpuDialog() {
     var url   = 'Function/systemManagement/overview/overview.php';
     var title = '多核处理器下各物理/逻辑核的利用率';
@@ -9,11 +10,29 @@ function openAllCpuDialog() {
         $(this).remove();
     };
     var dialogParams = {
-        width   : 700,
-        height  : 400,
+        width   : 850,
+        height  : 560,
         buttons : buttons,
         position: ['left', 'top']
     };    
-    showDialogByAjax(url, data, title, dialogParams);
+    opendialog=showDialogByAjax(url, data, title, dialogParams,"",successCallback);
+	function successCallback(result,textStatus){
+		opendialog.setContent(result.msg);
+	}
+}
 
+function setConOption(w,h){
+	if(!w) w=850;
+	if(!h) h=560;
+	var buttons = {};
+    buttons['取消'] = function() {
+        $(this).remove();
+    };
+    var dialogParams = {
+        width   : w,
+        height  : h,
+        buttons : buttons,
+        position: ['left', 'top']
+    };   
+	opendialog.setOptions(dialogParams)
 }
